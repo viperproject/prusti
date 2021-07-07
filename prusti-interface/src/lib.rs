d32e8640beec33b83ea5d3a421a1300a7971d100
Vytautas Astrauskas <vastrauskas@gmail.com>
2021-07-07 16:59:13 +0200

    Update to support the latest version of the compiler.

--- prusti-interface/src/lib.rs
+++ prusti-interface/src/lib.rs
@@ -40,6 +40,7 @@ extern crate polonius_engine;
 // extern crate rustc;
 extern crate rustc_data_structures;
 extern crate rustc_index;
+extern crate rustc_trait_selection;
 // extern crate rustc_driver;
 // extern crate rustc_hash;
 extern crate rustc_mir;
