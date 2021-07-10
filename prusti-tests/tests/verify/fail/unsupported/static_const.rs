8878c7352b504432f5470734e2722154a24c16e2
Vytautas Astrauskas <vastrauskas@gmail.com>
2021-07-10 18:12:13 +0200

    Fix test.

--- prusti-tests/tests/verify/fail/unsupported/static_const.rs
+++
@@ -1,8 +0,0 @@
-const fn foo() -> usize {
-    1
-}
-
-fn main() { //~ ERROR: [Prusti internal error] tried to encode a projection that accesses the field 0 of a variant without first downcasting its enumeration std::option::Option<std::fmt::Arguments
-    let bar: [usize; foo()] = [1];
-    assert_eq!(bar[0], foo());
-}
